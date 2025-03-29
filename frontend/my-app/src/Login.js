5cc01ff455be4e50951f86b11a5c0ede5d343d46
Tomasz <tom.pl@op.pl>
2025-03-29 16:27:46 +0100

    Frontend pages specifics

--- frontend/my-app/src/Login.js
+++ frontend/my-app/src/Login.js
@@ -13,11 +13,11 @@ function LoginComponent() {
     }
 
     return (
-        <section className="vh-100 gradient-custom">
+        <section className="vh-100" >
             <div className="container py-5 h-100">
                 <div className="row d-flex justify-content-center align-items-center h-100">
                     <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-                        <div className="card bg-dark text-white" style={{"borderRadius":"1em"}}>
+                        <div className="card text-white" style={{"borderRadius":"1em", "backgroundColor":"#0E021D", "borderWidth":"1px", "borderStyle":"solid"}}>
                             <div className="card-body p-5 text-center">
 
                                 <form className="mb-md-5 mt-md-4 pb-5" onSubmit={handleLogin}>
@@ -26,8 +26,8 @@ function LoginComponent() {
                                     <p className="text-white-50 mb-5">Please enter your username</p>
 
                                     <div data-mdb-input-init className="form-outline form-white mb-4">
-                                        <label className="form-label" htmlFor="typeNameX">Username</label>
-                                        <input type="name" id="typeNameX" className="form-control form-control-lg" />
+                                        <label className="form-label" htmlFor="typeNameX" >Username</label>
+                                        <input type="name" id="typeNameX" className="form-control form-control-lg" style={{"background":"transparent", "color":"white"}}/>
                                     </div>
 
                                     <button data-mdb-button-init data-mdb-ripple-init className="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
