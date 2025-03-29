b6339d713ec325278c968da6d0ad4743e3c1ae28
Tomasz <tom.pl@op.pl>
2025-03-29 14:57:21 +0100

    Leaderboard basics

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,20 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-import Navbar from './Navbar';
-import NavbarComponent from './Navbar';
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginComponent from './Login';
 import RoutesComponent from './Routes';
 import NoPageComponent from './NoPage';
 import Wrapper from './SiteWrapper';
+import LeaderboardsComponent from './Leaderboards';
 
 function App() {
+
   return (
     <div className="App bg-transparent">
       <BrowserRouter>
         <Routes>
-          <Route path='routes'element={< Wrapper component={RoutesComponent } />}/>
+          <Route path='routes' element={< Wrapper component={RoutesComponent } />}/>
+          <Route path='leaderboard' element={<Wrapper component={LeaderboardsComponent} />} />
           <Route path='login' element={ <LoginComponent />} />
           <Route path='*' element={<Wrapper component={NoPageComponent} />} />
         </Routes>
