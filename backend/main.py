88f1c2a1ec89a95af063ef91553db5d4fe306c24
michal1mroz <michalm12627@gmail.com>
2025-03-29 13:16:04 +0100

    Added user table

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
-from typing import Annotated
-from db.models import Route
+from typing import Annotated, Optional
+from db.models import Route, User
 from db.setup import SessionDep, create_db_and_tables
 from fastapi import FastAPI, Query
 from dotenv import load_dotenv
@@ -14,6 +14,7 @@ GOOGLE_MAPS_API_KEY = os.getenv("GOOGLE_MAPS_API_KEY")
 @app.on_event("startup")
 def on_startup() -> None:
     create_db_and_tables()
+    
 
 @app.get("/")
 def routes() -> list[str]:
@@ -34,3 +35,28 @@ def list_all_routes(
 ) -> list[Route]:
     routes = session.exec(select(Route).offset(offset).limit(limit)).all()
     return routes
+
+@app.get("/user/{user_id}")
+def get_user_by_id(
+    session: SessionDep, 
+    user_id: int,
+    offset: int = 0,
+) -> Optional[User]:
+    user = session.exec(select(User).where(User.id == user_id).offset(offset)).first()
+    return user 
+
+@app.get("/user")
+def list_all_users(
+    session: SessionDep, 
+    limit: Annotated[int, Query(le=100)] = 100,
+    offset: int = 0,
+) -> list[User]:
+    
+    return session.exec(select(User).offset(offset).limit(limit)).all()
+
+@app.post("/user")
+def add_user(user: User, session: SessionDep) -> None:
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
